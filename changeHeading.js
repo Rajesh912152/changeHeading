30c29a8f4c30fefdf5e929602e92f3f364324d08
Rajesh912152 <121996259+Rajesh912152@users.noreply.github.com>
2023-12-20 16:45:15 +0530

    Add files via upload

---
+++ changeHeading.js
@@ -0,0 +1,10 @@
+let headingEl=document.getElementById("headingElement");
+function manipulateStyle(){
+    headingEl.textContent="4.0 Technologies";
+    headingEl.style.color="Red";
+}
+
+function manipulateStyle2(){
+    headingEl.textContent="Web Technologies";
+    headingEl.style.color="Black";
+}
